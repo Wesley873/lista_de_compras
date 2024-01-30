bb9c2140bb30eceb5c75ddca79ec6d211c8a628b
Wesley Araujo <wesley.araujo@onpagtech.com.br>
2024-01-30 12:40:44 -0300

    Mais um teste

--- src/js/pagina2.js
+++ src/js/pagina2.js
@@ -1,12 +1,6 @@
-import fs from 'fs';
 document.addEventListener('click', () => {
-    fs.readFile('../index.html', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
-  
+    console.log('Click realizado com sucesso.');
 });
+
+
 
