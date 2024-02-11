45bba4772eff9bb8217bba2cec720e416852d6f5
Wesley Araujo <wesley.araujo@onpagtech.com.br>
2024-02-11 13:51:58 -0300

    mudou um monte de coisa

--- src/js/index.js
+++ src/js/index.js
@@ -2,45 +2,89 @@ import Produto from './produto.js';
 const listaProdutos = [];
 
 const saldo = document.querySelector('#saldo');
-const lista = document.querySelector('#lista');
-const item = document.querySelector('#item');
-const preco = document.querySelector('#preco');
-const quantidade = document.querySelector('#quantidade');
-const unidade = document.querySelector('#unidade');
-const embalagem = document.querySelector('#embalagem');
 const disponivel = document.querySelector('#disponivel');
-const tabela = document.querySelector('#tabela');
-const tabelaCorpo = document.querySelector('#corpo-tabela');
+const tabela = document.querySelector('#tabela-lista');
+const CorpoTabela = document.querySelector('#corpo-tabela');
 const total = document.querySelector('#total');
+const fechar = document.querySelector('#fechar');
+const salvar = document.querySelector('#salvar');
+const adicionar = document.querySelector('#adicionar')
+const modal = document.querySelector('#modal');
 
-function adicionaItem(item, nome, preco, quantidade, embalagem, unidade){
-    const produto = new Produto(item, nome, preco, quantidade, embalagem, unidade);
-    listaProdutos.push(produto);
-    console.log(listaProdutos);
+
+fechar.onclick = () => {
+    modal.close();
 }
 
-function calculaSubtotal(){
+function adicionaItem(item, nome, preco, quantidade, embalagem, unidade) {
+    const produto = new Produto(item, nome, preco, quantidade, embalagem, unidade);
+    listaProdutos.push(produto);
+}
+
+function calculaSubtotal() {
     let subtotal = 0;
-    for(let produto of listaProdutos){
+    for (let produto of listaProdutos) {
         subtotal += produto.price * produto.quantity;
     }
-    total.value = `R$ ${subtotal}`;
-    saldo.value = `R$ ${disponivel.value - subtotal}`;
+    total.value = `R$ ${subtotal.toFixed(2)}`;
+    saldo.value = `R$ ${(disponivel.value - subtotal).toFixed(2)}`;
 };
 
-document.addEventListener('click', (e) => {
-    if(e.target.id === 'adicionar'){
-        tabela.classList.remove('oculto');
-        adicionaItem(listaProdutos.length + 1, item.value, preco.value, quantidade.value, embalagem.value, unidade.value);
-        calculaSubtotal();
-        tabelaCorpo.innerHTML = '';
-        for(let produto of listaProdutos){
-            const linha = `<tr><td>${produto.item} → </td><td>${produto.name}</td><td>R$ ${produto.price}</td><td>${produto.quantity}</td><td>${produto.packaging} ${produto.unitOfMeasurement}</td><td>R$ ${produto.price * produto.quantity}</td></tr>`
-            tabelaCorpo.innerHTML += linha;
-        }
-        item.value = '';
-        quantidade.value = '';
-        preco.value = '';
-        embalagem.value = '';
-    };
+function atualizaLista() {
+    tabela.classList.remove('oculto');
+    CorpoTabela.innerHTML = ``;
+    CorpoTabela.innerHTML = listaProdutos.map((produto) => {
+        return `
+            <tr>
+                <td><button type="button" class="editar-produto" id="${produto.item}">${produto.item}</button> </td>
+                <td>${produto.name}</td>
+                <td>R$ ${produto.price}</td>
+                <td>${produto.quantity}</td>
+                <td>${produto.packaging} ${produto.unitOfMeasurement}</td>
+                <td>R$ ${(produto.price * produto.quantity).toFixed(2)}</td>                
+            </tr>
+        `
+    }).join('');
+}
+
+adicionar.onclick = function () {
+    modal.showModal();
+    document.querySelector('#titulo-modal').innerText = 'Adicionar item';
+}
+
+salvar.onclick = function () {
+    adicionaItem(
+        listaProdutos.length + 1,
+        document.querySelector('#nome').value,
+        document.querySelector('#preco').value,
+        document.querySelector('#quantidade').value,
+        document.querySelector('#embalagem').value,
+        document.querySelector('#medida').value
+    );
+
+    document.querySelector('#nome').value = '';
+    document.querySelector('#preco').value = '';
+    document.querySelector('#quantidade').value = '';
+    document.querySelector('#embalagem').value = '';
+    // document.querySelector('#medida').value = '';
+
+    calculaSubtotal();
+    atualizaLista();
+    modal.close();
+}
+
+document.addEventListener('click', (evento) => {
+    const id = evento.target.id
+    if (evento.target.classList[0] === 'editar-produto') {
+        modal.showModal();
+        document.querySelector('#titulo-modal').innerText = `Editar item ${id}`;
+        document.querySelector('#nome').value = listaProdutos[id-1].name;
+        document.querySelector('#preco').value = listaProdutos[id-1].price;
+        document.querySelector('#quantidade').value = listaProdutos[id-1].quantity;
+        document.querySelector('#embalagem').value = listaProdutos[id-1].packaging;
+        document.querySelector('#medida').value = listaProdutos[id-1].unitOfMeasurement;
+
+        console.log(listaProdutos[id-1]);
+    }
+
 });
